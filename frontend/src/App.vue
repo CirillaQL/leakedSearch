456910e6bcbd665d02b8073d1c3d5b0fb0eb18c5
lu.qian <1194946223@qq.com>
2022-06-26 20:37:07 +0800

    Init frontend project by vue3 and use naive UI.

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="logo"  src="./assets/leakedSearchLogo.png">
+  <div class="blank"/>
+  <SearchInput/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import SearchInput from './components/SearchInput.vue';
+
+@Options({
+  components: {
+    SearchInput,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.blank{
+  margin: 5em auto 5em;
+}
+</style>
---
+++ frontend/src/components/SearchInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+    <n-space vertical>
+      <n-input-group>
+        <n-input placeholder="搜索内容" style="width: 50%;">
+          <template #suffix>
+          </template>
+        </n-input>
+        <n-button :loading="loading" @click="handleClick">
+          搜索
+        </n-button>
+      </n-input-group>
+    </n-space>
+  </n-config-provider>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref } from 'vue'
+  import { NConfigProvider, NInput, NSpace, NButton } from 'naive-ui'
+
+  export default defineComponent({
+    components: {
+      NConfigProvider,
+      NInput,
+      NSpace,
+      NButton
+    },
+    setup() {
+      const loadingRef = ref(false)
+      return {
+        handleClick(){
+          loadingRef.value = true
+          setTimeout(() => {
+            loadingRef.value = false
+          }, 2000)
+        },
+        loading: loadingRef,
+      }
+    }
+  })
+</script>
